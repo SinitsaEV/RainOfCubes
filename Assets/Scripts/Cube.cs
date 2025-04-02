1c18e67aa31a77ad8b13dbf62dbaab113b82eed0
SinitsaEV <sinitsammi@gmail.com>
2025-04-02 16:35:31 +0300

    RAIN

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -1,18 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Cube : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] LayerMask _layerMask;
+
+    private ColorChanger _colorChanger;
+    private Spawner _spawner;
+    private Renderer _renderer;
+
+    private Color _defaultСolor;
+    private float _lifeTime;
+    private bool isFirstCollision = true;
+
+    private void Awake()
     {
-        
+        _colorChanger = FindObjectOfType<ColorChanger>();
+        _renderer = GetComponent<Renderer>();
+        _spawner = FindObjectOfType<Spawner>();
+        _defaultСolor = _renderer.material.color;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (( _layerMask.value & (1 << collision.gameObject.layer)) != 0 && isFirstCollision)
+        {
+            isFirstCollision = false;
+            _renderer.material.color = _colorChanger.GetRandomColor();
+            Invoke(nameof(ReturnCube), _lifeTime);
+        }
+    }
+
+    public void ResetCube(float lifeTime)
+    {
+        _renderer.material.color = _defaultСolor;
+        isFirstCollision = true;
+        _lifeTime = lifeTime;
+    }
+
+    private void ReturnCube()
+    {
+        _spawner.ReturnCube(gameObject);
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,18 +1,73 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
+
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject _prefab;
+
+    [SerializeField] private float _height;
+    [SerializeField] private MeshCollider _spawnPlaceMesh;
+
+    [SerializeField] private float _delay = 1f;
+    private WaitForSecondsRealtime _time;
+
+    private ObjectPool<GameObject> _pool;
+    private int _poolCapacity = 10;
+    private int _poolDefaultCapacity = 10;
+
+    private float _maxLifeTime = 5f;
+    private float _minLifeTime = 2f;
+
+    private void Awake()
     {
-        
+        _time = new WaitForSecondsRealtime(_delay);
+        _pool = new ObjectPool<GameObject>(
+            createFunc: () => Instantiate(_prefab),
+            actionOnGet: (obj) => ActionOnGet(obj),
+            actionOnRelease: (obj) => obj.SetActive(false),
+            actionOnDestroy: (obj) => Destroy(obj),
+            collectionCheck: true,
+            defaultCapacity: _poolDefaultCapacity,
+            maxSize: _poolCapacity);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        InvokeRepeating(nameof(GetCube), 0.0f, _delay);
+    }
+
+    public void ReturnCube(GameObject cube)
+    {
+        _pool.Release(cube);
+    }
+
+    private void GetCube()
+    {
+        _pool.Get();
+    }
+
+    private void ActionOnGet(GameObject obj)
+    {
+        obj.transform.position = GetRandomSpawnPosition();
+        obj.transform.rotation = Quaternion.Euler(Vector3.zero);
+        obj.GetComponentInChildren<Rigidbody>().velocity = Vector3.zero;
+        obj.SetActive(true);
+        obj.GetComponentInChildren<Cube>().ResetCube(GetRandomLifeTime());
+    }
+
+    private Vector3 GetRandomSpawnPosition()
+    {
+        Bounds bounds = _spawnPlaceMesh.bounds;
+
+        float randomX = Random.Range(bounds.min.x, bounds.max.x);
+        float randomZ = Random.Range(bounds.min.z, bounds.max.z);
+
+        return new Vector3(randomX, _height + bounds.max.y, randomZ);
+    }
+
+    private float GetRandomLifeTime()
+    {
+        return Random.Range(_minLifeTime, _maxLifeTime);
     }
 }
